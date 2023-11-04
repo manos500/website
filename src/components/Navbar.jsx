2061a766f2d1acd3d7c6be2cfb5de59f426ffd2a
Manos Nikitakis <manosnikitakis3@gmail.com>
2023-11-04 20:30:53 +0200

    added animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
       };
     }, [windowWidth]); 
   return (
-    <nav className={`${styles.paddingX} w-full flex flex-col items-center py-5 fixed top-0 z-20 bg-primary box_shadow_b`}>
+    <nav className={`${styles.paddingX} w-full flex flex-col items-center py-5 fixed top-0 z-20 bg-primary box_shadow_b test`}>
            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
             <Link to='/' className="flex items-center gap-2" onClick={() => {
                 setActive('');
