92c9ba7c70c31a076ef874f6b31ef551d0974db2
Manos Nikitakis <manosnikitakis3@gmail.com>
2023-11-07 16:05:29 +0200

    add images for export

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,6 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -20,15 +19,16 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-
+import unipi_logo from "./unipi_logo.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import france from "./france.png";
+import united_kingdom from "./united_kingdom.png";
 
 export {
   logo,
@@ -59,4 +59,8 @@ export {
   carrent,
   jobit,
   tripguide,
+  unipi_logo,
+  france,
+  united_kingdom,
+
 };
