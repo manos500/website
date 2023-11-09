27ab52b34817cfdf30f0ca6a1821e50856c53910
Manos Nikitakis <manosnikitakis3@gmail.com>
2023-11-09 10:15:18 +0200

    modified assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,33 +3,12 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
-import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import unipi_logo from "./unipi_logo.png";
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import france from "./france.png";
 import united_kingdom from "./united_kingdom.png";
-import github2 from "./github2.svg";
+import github from "./github.png";
 import twitter from "./twitter.png";
 import instagram from "./instagram.svg";
 
@@ -42,31 +21,11 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   unipi_logo,
   france,
   united_kingdom,
   twitter,
   instagram,
-  github2,
+
 
 };
