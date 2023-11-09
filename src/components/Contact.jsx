bfa5c058ebb30cba3e4b7a6ea706db5015f95a06
Manos Nikitakis <manosnikitakis3@gmail.com>
2023-11-09 09:52:39 +0200

    added footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,24 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
+import { socialMedia } from '../constants'
 
 import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="w-full flex md:flex-row flex-col items-center justify-between pt-6 mb-[14px] box_shadow_t">
+      <p className="font-poppins font-normal text-[16px] leading-[20px] text-center text-white">Copyright © 2023 Manos, All Rights Reserved.</p>
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social,index) => (
+          <a href={`${social.link}`} target="_blank">
+            <img key={social.id} src={social.icon} alt="social_media" className={`md:w-[35px] md:h-[35px] w-[27px] h-[27px] object-contain cursor-pointer hover:opacity-70 md:hover:w-[36px] md:hover:h-[36px] hover:w-[28px] hover:h-[28px] ${index !== socialMedia.length ? "mr-6" : "mr-0"} `}/>
+          </a>
+        ))}
+      </div>
+    </div>
   )
 }
 
